ee528b94c3aeae365d140fd801d515d266260c81
NBurns90 <natburns@gmail.com>
2018-03-20 21:17:13 +0000

    problem2 progress

--- problem2/lib/problem2.rb
+++ problem2/lib/problem2.rb
@@ -3,20 +3,19 @@ class Problem
   attr_accessor :num_arr
 
   def initialize
-    @num_arr = [1, 2]
+    @num_arr = []
     @even_arr = []
   end
 
-  def iterator
-
+  def iterator(f,s)
+    num_arr[0] = f
+    num_arr[1] = s
     while @num_arr.last < 4000000
       addition = @num_arr.last(2).sum
-      num_arr.push(addition)
+      @num_arr.push(addition)
       evenchecktoarray(@num_arr.last)
     end
 
-    puts @num_arr
-    puts @even_arr
   end
 
   def evenchecktoarray(i)
@@ -26,11 +25,11 @@ class Problem
   end
 
   def answer
-    puts @even_arr.sum + 2
+    puts @even_arr.sum
   end
 
 end
 
 x = Problem.new
-puts x.iterator()
+puts x.iterator(0, 1)
 x.answer
