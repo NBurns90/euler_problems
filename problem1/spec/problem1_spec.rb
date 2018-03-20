dd80fec11b263746f65abb026caea4eff18c62c9
NBurns90 <natburns@gmail.com>
2018-03-20 22:40:09 +0000

    Added Readme and a couple of changes

--- problem1/spec/problem1_spec.rb
+++ problem1/spec/problem1_spec.rb
@@ -6,8 +6,8 @@ describe Problem do
     expect(Problem.new.range(1, 100)).to cover(1, 99, 100)
   end
 
-  it "should divide 10 by 2" do
-    expect(Problem.new.divisible(10, 2)).to eq(true)
+  it "should divide 10 by 5" do
+    expect(Problem.new.divisible(10, 5)).to eq(true)
   end
 
   it "should divide 12 by 3" do
--- problem2/spec/problem2_spec.rb
+++ problem2/spec/problem2_spec.rb
@@ -6,9 +6,9 @@ describe Problem do
     test = Problem.new
   end
 
-  it "should check if number is even and push to evenarray" do
-    expect(test.iterator(1,2)).to eq([10])
-  end
+  # it "should check if number is even and push to evenarray" do
+  #   expect(test.iterator(1,2)).to eq([10])
+  # end
 
   # it "should check array contains fib numbers" do
   #   expect(Problem.new.iterator(1,2).num_arr.to include(1, 2, 3, 5, 8, 13)
